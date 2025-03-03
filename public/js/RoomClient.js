637d3f8caedc61becf19c5973db1a409cfd286d4
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-03-03 18:00:06 +0100

    [mirotalksfu] - Fix encoding

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -756,17 +756,17 @@ class RoomClient {
             {
                 rid: 'r0',
                 maxBitrate: 100000,
-                scalabilityMode: 'S1T3',
+                scalabilityMode: 'L3T3',
             },
             {
                 rid: 'r1',
                 maxBitrate: 300000,
-                scalabilityMode: 'S1T3',
+                scalabilityMode: 'L3T3',
             },
             {
                 rid: 'r2',
                 maxBitrate: 900000,
-                scalabilityMode: 'S1T3',
+                scalabilityMode: 'L3T3',
             },
         ];
     }
